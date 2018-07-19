a976a4075f87402e83150c42dadee9d6822be500
JingYang <jingyang@tcsl.com.cn>
2018-07-19 15:54:56 +0800

    v2.9.3 fix code error in 2.9.2

--- app/src/main/java/com/king/app/vrace/utils/ColorUtil.java
+++ app/src/main/java/com/king/app/vrace/utils/ColorUtil.java
@@ -48,7 +48,7 @@ public class ColorUtil {
 	public static boolean isDeepColor(int color) {
 //		double darkness = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255;
 //		return darkness >= 0.5;
-		return Color.red(color) * 0.299 + Color.red(color) * 0.578 + Color.red(color) * 0.114 < 192;
+		return Color.red(color) * 0.299 + Color.green(color) * 0.578 + Color.blue(color) * 0.114 < 192;
 	}
 
 }
